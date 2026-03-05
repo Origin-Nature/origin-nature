a5580686a541497d9da97003c2dc6e3fa058ccfa
JD <98412971+jd-bar@users.noreply.github.com>
2026-03-05 12:07:08 +0100

    Remove SpeedInsights and Analytics from MyApp
    Removed SpeedInsights and Analytics components from MyApp.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
-      <Analytics />
     </>
   );
 }
