eb43194358f9d33d4aff5da1f49d73b0ab45bcb1
JD <98412971+jd-bar@users.noreply.github.com>
2026-03-07 13:49:06 +0100

    Add SpeedInsights and Analytics components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
     </>
   );
 }
