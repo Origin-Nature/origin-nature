457c9aca2399367ad0e38bb091fa21073fabab19
JD <98412971+jd-bar@users.noreply.github.com>
2026-05-14 07:55:32 +0200

    Update redirects in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,39 +4,26 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/post/:slug',
-        destination: '/',
+        source: '/:path((?!en|fr).*)',
+        destination: '/en',
         permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'origin-nature\\.com',
+          },
+        ],
       },
       {
-        source: '/about',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/about/:slug',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/brands',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/brands/:slug',
-        destination: '/?brand=:slug',
-        permanent: false,
-      },
-      {
-        source: '/materials',
-        destination: '/',
-        permanent: false,
-      },
-      {
-        source: '/materials/:slug',
-        destination: '/?material=:slug',
-        permanent: false,
+        source: '/:path((?!en|fr).*)',
+        destination: '/fr',
+        permanent: true,
+        has: [
+          {
+            type: 'host',
+            value: 'origine-nature\\.com',
+          },
+        ],
       },
     ];
   },
